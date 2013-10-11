ac35b51888e92eddd6d09f9e55fd3d30250ab674
Michael Bauer <mihi@lo-res.org>
2013-10-11 19:09:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+A proxy for DataPackages
+========================
+
+DataPackageProxy?
+-----------------
+
+[DataPackages](http://data.okfn.org/standards/data-package) are a nice and
+neat tool to work with and bundle Data. However, working with them in
+browser-based apps is not straight forward. People's hosting doesn't
+support [CORS](http://enable-cors.org) so including datapackag files using
+e.g. d3.csv does not work :/. *Datapackageproxy* to the rescue!
+
+DatapackageProxy is a appengine based script, that takes a datapackage url
+(without the trailing /datapackage.json) and allows you to access it's
+resources.
+
+e.g:
+
+```
+http://datapackageproxy.appengine.com/resources?n=0&url=http://data.okfn.org/data/bond-yields-uk-10y
+```
+
+shows the first resource in the datapackage. If you're not sure which
+resource to use, try:
+
+```
+http://datapackageproxy.appengine.com/metadata?url=http://data.okfn.org/data/bond-yields-uk-10y
+```
+
+This will return the metadata for the package.
+
+Builds on the [datapackage python module](http://github.com/tryggvib/datapackage)
---
+++ dproxy.py
@@ -0,0 +1,74 @@
+import csv
+import io
+import json
+import re
+import webapp2
+from google.appengine.api import urlfetch
+from datapackage import DataPackage
+from google.appengine.api import memcache
+
+class Resource(webapp2.RequestHandler):
+  def get(self):
+    url=self.request.get('url',None)
+    id=self.request.get('id','')
+    if url:
+      if not re.match(".*?/$",url):
+        url="%s/"%url
+      data=memcache.get('resource-%s-%s'%(url,id))
+      self.response.headers['Content-Type'] = 'application/CSV; charset=utf-8;'
+      self.response.headers['Access-Control-Allow-Origin'] = '*'
+      if not data:  
+        d=DataPackage("%s"%url)
+        try:
+          data=[i for i in d.data]
+          cols=data[0].keys()
+          with io.BytesIO() as sio:
+            w=csv.writer(sio)
+            w.writerow(cols)
+            for i in data:
+              w.writerow([i[x] for x in cols])
+            memcache.add('resource-%s-%s'%(url,id),sio.getvalue(),300)
+            self.response.write(sio.getvalue())
+        except ValueError:
+          self.response.write("""Decoding of the package failed - this means
+          either the resource does not exist - or more likely the data
+          contains errors (numbers that are not numbers, dates that cannot
+          be parsed etc.)""")
+      else:
+        self.response.write(data)  
+    else:
+      self.response.write("Error: please specify URL")
+      
+    
+
+class MetaData(webapp2.RequestHandler):
+  def get(self):
+    url=self.request.get('url',None)
+    if url:
+      if not re.match(".*?/$",url):
+        url="%s/"%url
+      data=memcache.get('metadata-%s'%url)
+      self.response.headers['Content-Type'] = 'application/json; charset=utf-8;'
+      self.response.headers['Access-Control-Allow-Origin'] = '*'
+      if not data:
+        d=DataPackage(url)
+        memcache.add('metadata-%s'%url,json.dumps(d.get_descriptor()),300)
+        self.response.write(json.dumps(d.get_descriptor()))
+      else:
+        self.response.write(data)
+    else:
+      self.response.write("Error: please specify URL")
+    
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/html; charset=utf-8;'
+    with open("index.html") as f:
+      self.response.write(f.read())
+
+
+application=webapp2.WSGIApplication( [
+  ('/', MainPage),
+  ('/resource', Resource),
+  ('/metadata', MetaData),
+  ],debug=True)
